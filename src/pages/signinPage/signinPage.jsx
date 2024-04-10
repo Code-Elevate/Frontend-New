4900884614d9e2397558ce3999aac424f4cc0d3a
Parth Sali <parthsali04@gmail.com>
2024-04-10 19:16:55 +0530

    Auth : Fixed Minor Styling Bugs

--- src/pages/signinPage/signinPage.jsx
+++ src/pages/signinPage/signinPage.jsx
@@ -1,8 +1,6 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { toast } from 'react-hot-toast';
-
-
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
 const SignInPage = () => {
   const [user, setUser] = useState({});
@@ -20,29 +18,32 @@ const SignInPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch('https://code-elevate.onrender.com/api/users/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-      });
+      const res = await fetch(
+        "https://code-elevate.onrender.com/api/users/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(user),
+        }
+      );
 
-      const authToken = res.headers.get('x-auth-token');
+      const authToken = res.headers.get("x-auth-token");
       const data = await res.json();
 
       if (res.status === 200 && authToken) {
-        localStorage.setItem('x-auth-token', authToken);
-        localStorage.setItem('x-user-data', JSON.stringify(data.user));
-        localStorage.setItem('x-profile-data', JSON.stringify(data.profile));
-        toast.success('Login successful!');
-        navigate('/');
+        localStorage.setItem("x-auth-token", authToken);
+        localStorage.setItem("x-user-data", JSON.stringify(data.user));
+        localStorage.setItem("x-profile-data", JSON.stringify(data.profile));
+        toast.success("Login successful!");
+        navigate("/");
       } else {
         toast.error(data.message);
       }
     } catch (error) {
-      toast.error('An error occurred during login.');
-      console.error('Error logging in:', error);
+      toast.error("An error occurred during login.");
+      console.error("Error logging in:", error);
     }
   };
 
@@ -52,20 +53,55 @@ const SignInPage = () => {
         <h2 className="text-2xl font-bold mb-4 text-center">Sign In</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-            <input id="email" type="email" name="email" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50" onChange={handleChange} />
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              className="mt-1 py-1 px-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 outline-none"
+              onChange={handleChange}
+            />
           </div>
           <div>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-            <input id="password" type="password" name="password" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50" onChange={handleChange} />
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              name="password"
+              className="mt-1 py-1 px-2 outline-none block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+              onChange={handleChange}
+            />
           </div>
           <div className="text-right">
-            <a href="#" className="text-sm text-indigo-600 hover:underline">Forgot password?</a>
+            <a href="#" className="text-sm text-indigo-600 hover:underline">
+              Forgot password?
+            </a>
           </div>
-          <button type="submit" className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700">Sign In</button>
+          <button
+            type="submit"
+            className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700"
+          >
+            Sign In
+          </button>
         </form>
         <div className="mt-4 text-center">
-          <span className="text-sm text-gray-600">Don't have an account?</span> <Link to="/signup" className="text-sm text-indigo-600 hover:underline">Create account</Link>
+          <span className="text-sm text-gray-600">Don't have an account?</span>{" "}
+          <Link
+            to="/signup"
+            className="text-sm text-indigo-600 hover:underline"
+          >
+            Create account
+          </Link>
         </div>
       </div>
     </div>
--- src/pages/signupPage/signupPage.jsx
+++ src/pages/signupPage/signupPage.jsx
@@ -31,29 +31,31 @@ const SignUpPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    
     if (user.password.length < 6) {
       toast.error("Password must be at least 6 characters long.");
       return;
     }
 
     try {
-      const res = await fetch("https://code-elevate.onrender.com/api/users/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user),
-      });
+      const res = await fetch(
+        "https://code-elevate.onrender.com/api/users/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(user),
+        }
+      );
 
       const data = await res.json();
 
       if (res.status === 200) {
         setSuccessMessage("Registration successful!");
         setTimeout(() => {
-          setSuccessMessage(""); 
-          navigate("/"); 
-        }, 3000); 
+          setSuccessMessage("");
+          navigate("/");
+        }, 3000);
       } else {
         toast.error(data.message || "An error occurred during registration.");
       }
@@ -72,28 +74,92 @@ const SignUpPage = () => {
         )}
         <form className="space-y-4" onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
-            <input id="name" type="text" name="name" value={user.name} onChange={handleChange} className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50" required />
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Name
+            </label>
+            <input
+              id="name"
+              type="text"
+              name="name"
+              value={user.name}
+              onChange={handleChange}
+              className="mt-1 py-1 px-2 outline-none block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+              required
+            />
           </div>
           <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-            <input id="email" type="email" name="email" value={user.email} onChange={handleChange} className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50" required />
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              value={user.email}
+              onChange={handleChange}
+              className="mt-1 py-1 px-2 outline-none block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+              required
+            />
           </div>
           <div>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-            <input id="password" type="password" name="password" value={user.password} onChange={handleChange} className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50" />
-            {passwordError && <p className="text-red-500 text-sm">{passwordError}</p>}
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              name="password"
+              value={user.password}
+              onChange={handleChange}
+              className="mt-1 py-1 px-2 outline-none block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+            />
+            {passwordError && (
+              <p className="text-red-500 text-sm">{passwordError}</p>
+            )}
           </div>
           <div>
-            <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">Confirm Password</label>
-            <input id="confirmPassword" type="password" name="confirmPassword" value={user.confirmPassword} onChange={handleChange} className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50" />
+            <label
+              htmlFor="confirmPassword"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Confirm Password
+            </label>
+            <input
+              id="confirmPassword"
+              type="password"
+              name="confirmPassword"
+              value={user.confirmPassword}
+              onChange={handleChange}
+              className="mt-1  py-1 px-2 outline-none block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+            />
           </div>
-          <button type="submit" className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700">Register</button>
-
+          <button
+            type="submit"
+            className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700"
+          >
+            Register
+          </button>
         </form>
-        
+
         <div className="mt-4 text-center">
-          <span className="text-sm text-gray-600">Already have an account?</span> <Link to="/signin" className="text-sm text-indigo-600 hover:underline">Sign In</Link>
+          <span className="text-sm text-gray-600">
+            Already have an account?
+          </span>{" "}
+          <Link
+            to="/signin"
+            className="text-sm text-indigo-600 hover:underline"
+          >
+            Sign In
+          </Link>
         </div>
       </div>
     </div>
